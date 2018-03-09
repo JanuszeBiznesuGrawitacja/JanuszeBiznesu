c4900764b63599299b946b89ce61e37ca4d29038
Adam Waśniewski <tabuplaw@gmail.com>
2018-03-09 21:52:00 +0100

    Interactive Objects

---
+++ Assets/Scripts/JumpingPlatfrm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlatfrm : MonoBehaviour {
+
+    [SerializeField] float relativeVelocityToBreak = 1f;
+
+    [SerializeField] GameObject ourPipe;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.magnitude >= relativeVelocityToBreak)
+        {
+            print("wdwdwdwd");
+            Destroy(ourPipe);
+        }
+    }
+}
