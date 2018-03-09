632b30a47027758c271a9826d24e6eea1745676a
Mateusz Olszewski <olszewskim1991@gmail.com>
2018-03-10 00:01:17 +0100

    Players Manager refactoring

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PlayerController : MonoBehaviour {
+	private Platformer2DUserControl userControl;
+	private PlatformerCharacter2D platformerCharacter2D;
+	private Animator animator;
+
+	private void Awake() {
+		userControl = GetComponent<Platformer2DUserControl>();
+		platformerCharacter2D = GetComponent<PlatformerCharacter2D>();
+		animator = GetComponent<Animator>();
+		SetAsActivePlayer(false);
+	}
+
+	public void SetAsActivePlayer(bool state) {
+		userControl.enabled = state;
+		platformerCharacter2D.enabled = state;
+		animator.enabled = state;
+	}
+}
