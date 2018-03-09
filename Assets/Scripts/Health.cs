a09eaa06f006b9faabc5098fa37cc2d221072a6c
Mateusz Olszewski <olszewskim1991@gmail.com>
2018-03-09 20:25:41 +0100

    Basic character health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+	public Slider healthBar;
+	public float startHealthAmount = 30f;
+	private float _currentHealth;
+
+	public float CurrentHealth {
+		get
+		{
+			return _currentHealth;
+		}
+		set
+		{
+			_currentHealth = value;
+			if (_currentHealth < 0)
+			{
+				Die();
+			}
+			else
+			{
+				RefreshHealthBarUI();
+			}
+		}
+	}
+
+	private void RefreshHealthBarUI() {
+		healthBar.value = _currentHealth;
+	}
+
+	private void Start() {
+		_currentHealth = startHealthAmount;
+		healthBar.minValue = 0;
+		healthBar.maxValue = startHealthAmount;
+		healthBar.value = startHealthAmount;
+	}
+
+	public void LoseHealth(float amount) {
+		CurrentHealth -= amount;
+	}
+
+	public void AddHealth(float amount) {
+		CurrentHealth += amount;
+	}
+
+	private void Update() {
+		LoseHealth(Time.deltaTime);
+	}
+
+	private void Die() {
+		Destroy(gameObject);
+	}
+}
