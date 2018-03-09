c0052ea2b6dc9750acb3443559e8e4be8b36bb88
Bartosz Bąk <bartosz.bk04@gmail.com>
2018-03-09 21:00:59 +0100

    Change Player

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PlayerManager : MonoBehaviour {
+
+    public GameObject player1, player2;
+    bool changePlayer = false;
+
+    void Awake()
+    {
+        player1.GetComponent<Platformer2DUserControl>().enabled = true;
+        player2.GetComponent<Platformer2DUserControl>().enabled = false;
+        player1.GetComponent<PlatformerCharacter2D>().enabled = true;
+        player2.GetComponent<PlatformerCharacter2D>().enabled = false;
+        player1.GetComponent<Animator>().enabled = true;
+        player2.GetComponent<Animator>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) changePlayer = !changePlayer;
+        ChangePlayer(changePlayer);
+
+    }
+
+    void ChangePlayer(bool change)
+    {
+        if(change)
+        {
+            player1.GetComponent<Platformer2DUserControl>().enabled = false;
+            player2.GetComponent<Platformer2DUserControl>().enabled = true;
+            player1.GetComponent<PlatformerCharacter2D>().enabled = false;
+            player2.GetComponent<PlatformerCharacter2D>().enabled = true;
+            player1.GetComponent<Animator>().enabled = false;
+            player2.GetComponent<Animator>().enabled = true;
+        }
+        else
+        {
+            player1.GetComponent<Platformer2DUserControl>().enabled = true;
+            player2.GetComponent<Platformer2DUserControl>().enabled = false;
+            player1.GetComponent<PlatformerCharacter2D>().enabled = true;
+            player2.GetComponent<PlatformerCharacter2D>().enabled = false;
+            player1.GetComponent<Animator>().enabled = true;
+            player2.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+
+
+}
