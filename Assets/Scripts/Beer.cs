c8926a7a9434f31fd9a8341399c4242332c47f3b
Adam Waśniewski <tabuplaw@gmail.com>
2018-03-10 01:43:05 +0100

    Assety do levela

---
+++ Assets/Scripts/Beer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beer : MonoBehaviour {
+
+    [SerializeField] float rotationSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector2(0,360*Time.deltaTime* rotationSpeed));
+	}
+}
