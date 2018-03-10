5afccb9a011b53f63ab77c72c63038aced0b6f5f
Bartosz Bąk <bartosz.bk04@gmail.com>
2018-03-10 12:10:41 +0100

    Te sciowa v2

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+    public float speed;
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb2d.velocity = Vector2.right * speed;
+    }
+}
