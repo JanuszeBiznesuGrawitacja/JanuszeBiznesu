17f11b059dc547f46b29777551a3be8c37e89fca
Mateusz Olszewski <olszewskim1991@gmail.com>
2018-03-10 03:06:02 +0100

    Garbage stack

---
+++ Assets/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour {
+	private Rigidbody2D rb2d;
+	private float damage = 5f;
+	private Vector2 yRange = new Vector2(40, 110);
+	private Vector2 xRange = new Vector2(150, 250);
+
+	private void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start() {
+		var force = new Vector2(Random.Range(yRange.x, yRange.y) * RandomSign(), Random.Range(xRange.x, xRange.y));
+		rb2d.AddForce(force);
+	}
+
+	private float RandomSign() {
+		return Random.value < .5 ? 1 : -1;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		var health = collision.gameObject.GetComponentInParent<Health>();
+		if (health)
+		{
+			health.LoseHealth(damage);
+			Destroy(gameObject);
+		}
+		else if (collision.gameObject.tag == "Floor")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GarbageStack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GarbageStack : MonoBehaviour {
+	public GameObject[] projectiles;
+	private float spawnEverySeconds = 2f;
+	private float timer;
+
+	private void Update() {
+		timer += Time.deltaTime;
+		if (timer >= spawnEverySeconds)
+		{
+			timer = 0;
+			ShootProjectile();
+		}
+	}
+
+	private void ShootProjectile() {
+		var projectilePrefab = projectiles[Random.Range(0, projectiles.Length)];
+		var rotation = Random.rotation.z;
+		var projectileGO = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, rotation));
+		projectileGO.SetActive(true);
+	}
+}
