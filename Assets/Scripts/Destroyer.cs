89de2076c20ef09e1170c872180bee4ee247acca
Mateusz Olszewski <olszewskim1991@gmail.com>
2018-03-10 03:59:04 +0100

    Destroyer

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		var health = collision.GetComponentInParent<Health>();
+		if (health)
+		{ health.Die(); }
+		else
+		{
+			Destroy(collision.gameObject);
+		}
+	}
+}
