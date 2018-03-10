9b7b655c280a54b2277eacff8038e4640c6f3f43
Mateusz Olszewski <olszewskim1991@gmail.com>
2018-03-10 06:50:38 +0100

    Clickable sounds

---
+++ Assets/Scripts/ClickableBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ClickableBehaviour : MonoBehaviour, IPointerClickHandler {
+	private Selectable s;
+
+	private void Awake() {
+		s = GetComponent<Selectable>();
+	}
+
+	public void OnPointerClick(PointerEventData eventData) {
+		if (s != null && s.interactable == false)
+			return;
+		SoundsManager.instance.PlayClickSound();
+	}
+}
--- Assets/Scripts/SoundsManager.cs
+++ Assets/Scripts/SoundsManager.cs
@@ -7,6 +7,7 @@ public class SoundsManager : MonoBehaviour {
 	private AudioSource audioSource;
 	public AudioSource[] musicSources;
 	public AudioClip pickPowerUp;
+	public AudioClip clickSound;
 
 	//Music Tracks
 	public AudioClip intro;
@@ -23,6 +24,10 @@ public class SoundsManager : MonoBehaviour {
 		audioSource = GetComponent<AudioSource>();
 		SetIntroMusicTrack();
 		DontDestroyOnLoad(gameObject);
+	}
+
+	public void PlayClickSound() {
+		PlaySound(clickSound);
 	}
 
 	public void PlayPickPowerUpSound() {
