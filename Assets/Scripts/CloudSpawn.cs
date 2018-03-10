9f2ce4d38479b29ccc72c0491ef4b38653ab0276
Bartosz Bąk <bartosz.bk04@gmail.com>
2018-03-10 08:15:32 +0100

    cloud

---
+++ Assets/Scripts/CloudSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawn : MonoBehaviour {
+
+    [SerializeField] Transform spawnPoint;
+    public Cloud[] cloud;
+
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            Instantiate(cloud[Random.Range(0, cloud.Length - 1)], new Vector2(Random.Range(spawnPoint.position.x-2,spawnPoint.position.x+2), Random.Range(spawnPoint.position.y - 2, spawnPoint.position.y + 2)), Quaternion.identity);
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+}
